3da6ce4a807136e20ee6680716a19f422f073363
transtone <zm3345@126.com>
2021-09-05 20:24:14 +0800

    chore: update

---
+++ src/pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <view class="index">
+    <view>
+      <img src="" alt="">
+    </view>
+    {{ msg }}
+    <view class="btn">
+      <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
+    </view>
+    <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover" />
+
+
+    <view class="px-5 my-2 w-full">
+        <nut-button block plain type="primary" >跳转</nut-button>
+    </view>
+
+    <view class="px-5 box-border w-full">
+        <nut-button block plain type="primary" >跳转2</nut-button>
+    </view>
+
+  </view>
+</template>
+
+<script lang="ts">
+import { reactive, toRefs } from 'vue';
+export default {
+  name: 'Index',
+  components: {
+    
+  },
+  setup(){
+    const state = reactive({
+      msg: '欢迎使用 NutUI3.0 开发小程序',
+      msg2: '你成功了～',
+      type: 'text',
+      show: false,
+      cover: false
+    });
+
+    const handleClick = (type: string, msg: string, cover: boolean = false) => {
+      state.show = true;
+      state.msg2 = msg;
+      state.type = type;
+      state.cover = cover;
+    };
+
+    return {
+      ...toRefs(state),
+      handleClick
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
